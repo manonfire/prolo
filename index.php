8fcb7a4dd2b667136bb97423c3141ece0513fb7e
Jorge Aguayo desde mackbook pro 2 <jaguay@yahoo.com>
2017-09-16 18:21:13 -0500

    Revert "Revert "siguen las pruebas""
    This reverts commit bc848aa702e6af54b74d4a788f150d2d03a5f832.

--- index.php
+++ index.php
@@ -2,12 +2,10 @@
     <title>
         Programa de lealtad Prata Joyeria Mexicana
     </title>
-    
     <head>
     </head>
-    
+
     <body>
         Hola mundo, ahi la llevo
     </body>
-    
 </html>
