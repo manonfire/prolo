0b049a4403fefd3912d10e6cf9fe2baf82d9fd5f
Jorge Aguayo desde mackbook pro 2 <jaguay@yahoo.com>
2017-09-16 18:07:06 -0500

    siguen las pruebas
    de cómo usar los commits

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
     <title>
         Programa de lealtad Prata Joyeria Mexicana
     </title>
-    
     <head>
+         sigo haciendo pruebas
+        
     </head>
-    
+
     <body>
         Hola mundo
     </body>
-    
 </html>
